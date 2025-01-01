db08cf8ba6bbce6c5ee4ee4234f060914fa6ef83
luzhongren <liuguanyi2000@163.com>
2025-01-01 16:21:28 +0800

    feat:增加排序

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+void sort(int* arr, int size) {
+    for (int i  =0; i < size - 1; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (arr[i] > arr[j]) {
+                swap(arr[i], arr[j]);
+            }
+        }
+    }
+}
+
 int main()
 {
+    int arr[] = {5, ,4 ,3,2 ,1};
+    int size = sizeof(arr)/sizeof(int);
+    sort(arr, size);
     std::cout << "hello world" << std::endl;
 }
